e1fb0cf5927cf6c8eec21ae295e295a3b7f4bec7
6110406112 <pongphol.k@ku.th>
2020-09-14 15:16:45 +0700

    success config

--- src/main/java/atm/Main.java
+++ src/main/java/atm/Main.java
@@ -5,12 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 
 public class  Main {
     public static void main(String[] args) {
-//        DataSourceDB dataSourceDB = new DataSourceDB();
-//        DataSourceFile dataSourceFile = new DataSourceFile("customers.txt");
-//        Bank bank = new Bank("My Bank", dataSourceFile);
-//        ATM atm = new ATM(bank);
-//        AtmUI atmUI = new AtmUI(atm);
-//        atmUI.run();
+
 
         ApplicationContext context =
                 new AnnotationConfigApplicationContext(ATMConfing.class);
