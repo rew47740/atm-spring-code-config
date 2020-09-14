1b561ec832a3b0068920558633c57d465207bd55
6110406112 <pongphol.k@ku.th>
2020-09-14 15:05:16 +0700

    first commit

---
+++ src/main/java/atm/ATMConfing.java
@@ -0,0 +1,30 @@
+package atm;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class ATMConfing {
+
+    @Bean
+    public DataSource dataSource(){
+        return new DataSourceFile("customers.txt");
+    }
+
+    @Bean
+    public Bank bank(){
+        return new Bank("My Bank", dataSource());
+    }
+
+    @Bean
+    public  ATM atm(){
+        return new ATM(bank());
+    }
+
+    @Bean
+    public  AtmUI atmUI(){
+        return new AtmUI(atm());
+    }
+
+
+}
